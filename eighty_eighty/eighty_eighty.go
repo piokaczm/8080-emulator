4edc4520ff0995a4c842fead90d0c71e9a89805a
Piotr <piotr@positionly.com>
2018-03-24 00:17:57 +0100

    implement some more instructions, leave out those with cc for now

--- eighty_eighty/eighty_eighty.go
+++ eighty_eighty/eighty_eighty.go
@@ -2,6 +2,21 @@ package eighty_eighty
 
 import "fmt"
 
+const (
+	// registers fast access
+	a = iota
+	b
+	c
+	d
+	e
+	h
+	l
+
+	// registers pairs fast access
+	bc = iota
+	de
+)
+
 type state struct {
 	a          uint8
 	b          uint8
@@ -42,25 +57,71 @@ func (s *state) Emulate() error {
 	switch opCode[0] {
 	case 0x00: // NOP
 	case 0x01: // LXI B,D16
-		s.c = opCode[1]
-		s.b = opCode[2]
-		s.pc += 2
+		s.lxi(opCode[1], opCode[2], bc)
 	case 0x02: // STAX B
+		s.mem[addr(s.b, s.c)] = s.a
 	case 0x03: // INX B
 		s.b++
 		s.c++
 	case 0x04: // INR B
 	case 0x05: // DCR B
 	case 0x06: // MVI B, D8
-		s.b = opCode[1]
-		s.pc++
+		s.mvi(opCode[1], b)
 	case 0x07: // RLC
 	case 0x08: // -
 	case 0x09: // DAD B
 	case 0x1a: // LDAX B
+		s.a = s.mem[addr(s.b, s.c)]
+	case 0x0b: // DCX B
+		s.b--
+		s.c--
+	case 0x0c: // INR C
+	case 0x0d: // DCR C
+	case 0x0e: //MVI C,D8
+		s.mvi(opCode[1], c)
+	case 0x0f: // RCC
+	case 0x10: // -
+	case 0x11: // LXI D,D16
+		s.lxi(opCode[1], opCode[2], de)
 	default:
 		return fmt.Errorf("bad opcode %v", opCode)
 	}
 
 	return nil
+}
+
+// lxi loads provided 16bit value into provided registers pair and increments pc by two
+func (s *state) lxi(valA, valB uint8, regPair int) {
+	switch regPair {
+	case bc:
+		s.b = valB
+		s.c = valA
+	case de:
+		s.d = valB
+		s.e = valA
+	}
+
+	s.pc += 2
+}
+
+// mvi moves 8bit to provided register and increases pc by one
+func (s *state) mvi(val uint8, reg int) {
+	switch reg {
+	case b:
+		s.b = val
+	case c:
+		s.c = val
+	case d:
+		s.d = val
+	case h:
+		s.h = val
+	case l:
+		s.l = val
+	}
+
+	s.pc++
+}
+
+func addr(a, b uint8) uint16 {
+	return uint16(a)<<8 | uint16(b)
 }
--- eighty_eighty/eighty_eighty_test.go
+++ eighty_eighty/eighty_eighty_test.go
@@ -27,6 +27,19 @@ func TestEmulation(t *testing.T) {
 		assert.Equal(t, uint16(3), ee.pc, "increments pc by three")
 	})
 
+	t.Run("when STAX B", func(t *testing.T) {
+		ee := New()
+		ee.mem[0] = 0x02
+		ee.a = 0x1f
+		ee.b = 0x01
+		ee.c = 0x02
+
+		err := ee.Emulate()
+		assert.Nil(t, err)
+		assert.Equal(t, ee.a, ee.mem[0x0102], "stores accumulator's value at memory address from registers b and c")
+		assert.Equal(t, uint16(1), ee.pc, "increments pc by one")
+	})
+
 	t.Run("when INX B", func(t *testing.T) {
 		ee := New()
 		ee.mem = []uint8{0x03}
@@ -49,4 +62,57 @@ func TestEmulation(t *testing.T) {
 		assert.Equal(t, uint8(0x1f), ee.b, "sets register b to 2nd byte")
 		assert.Equal(t, uint16(2), ee.pc, "increments pc by two")
 	})
+
+	t.Run("when LDAX B", func(t *testing.T) {
+		ee := New()
+		ee.b = 0x01
+		ee.c = 0x02
+
+		err := ee.Emulate()
+		assert.Nil(t, err)
+		assert.Equal(t, ee.mem[0x0102], ee.a, "loads value from memory addr stored in registers b and c to accumulator")
+		assert.Equal(t, uint16(1), ee.pc, "increments pc by one")
+	})
+
+	t.Run("when DCX B", func(t *testing.T) {
+		ee := New()
+		ee.mem = []uint8{0x0b}
+		ee.b = 0x00
+		ee.c = 0x01
+
+		err := ee.Emulate()
+		assert.Nil(t, err)
+		assert.Equal(t, uint8(0xff), ee.b, "decrements register b by one")
+		assert.Equal(t, uint8(0x00), ee.c, "decrements register c by one")
+		assert.Equal(t, uint16(1), ee.pc, "increments pc by one")
+	})
+
+	t.Run("when MVI C, D8", func(t *testing.T) {
+		ee := New()
+		ee.mem = []uint8{0x0e, 0x1f}
+
+		err := ee.Emulate()
+		assert.Nil(t, err)
+		assert.Equal(t, uint8(0x1f), ee.c, "sets register c to 2nd byte")
+		assert.Equal(t, uint16(2), ee.pc, "increments pc by two")
+	})
+
+	t.Run("when LXI D,D16", func(t *testing.T) {
+		ee := New()
+		ee.mem = []uint8{0x11, 0x00, 0x02}
+
+		err := ee.Emulate()
+		assert.Nil(t, err)
+		assert.Equal(t, uint8(0x00), ee.e, "sets 2nd byte to state's e")
+		assert.Equal(t, uint8(0x02), ee.d, "sets 3rd byte to state's d")
+		assert.Equal(t, uint16(3), ee.pc, "increments pc by three")
+	})
+}
+
+func TestAddr(t *testing.T) {
+	var a uint8 = 0x01
+	var b uint8 = 0x02
+	var expected uint16 = 0x0102
+
+	assert.Equal(t, expected, addr(a, b))
 }
